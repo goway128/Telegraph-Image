a99d141da5d93cf083b3280c67a1067bf549d09a
goway128 <3756004436@qq.com>
2026-04-24 20:03:57 +0800

    密码

--- functions/upload.js
+++ functions/upload.js
@@ -2,21 +2,7 @@ import { errorHandling, telemetryData } from "./utils/middleware";
 
 export async function onRequestPost(context) {
     const { request, env } = context;
-
-const adminPassword = env.BASIC_PASS;
-
-    if (adminPassword) {
-        const userPassword = request.headers.get('Authorization');
-        if (userPassword !== adminPassword) {
-            return new Response(JSON.stringify({ error: 'Unauthorized' }), {
-                status: 403,
-                headers: { 'Content-Type': 'application/json' }
-            });
-        }
-    }
-
-}
-  
+ 
     try {
         const clonedRequest = request.clone();
         const formData = await clonedRequest.formData();
